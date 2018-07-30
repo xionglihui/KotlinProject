73254ab129b3301d52a465ace7bd83f4d4ea5ae2
xionglihui <xionglihui@vip.qq.com>
2018-07-30 13:52:34 +0800

    net
    net

---
+++ app/src/main/java/com/xiong/kotlin/dandan/entity/UserInfo.kt
@@ -0,0 +1,9 @@
+package com.xiong.kotlin.dandan.entity
+
+/**
+ * Created by xionglh on 2018/7/30
+ */
+class UserInfo {
+
+
+}
---
+++ app/src/main/java/com/xiong/kotlin/dandan/net/api/Api.kt
@@ -0,0 +1,8 @@
+package com.xiong.kotlin.dandan.net.api
+
+/**
+ * Created by xionglh on 2018/7/30
+ */
+class Api {
+
+}
